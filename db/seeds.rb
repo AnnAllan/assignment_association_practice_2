5b7fb5753d2c12cf09ec54beb60c3e88db440a3b
Ann Allan <ann.cristina.allan@gmail.com>
2017-07-10 19:20:54 -0400

    Add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#-----Destroy previous records-----
+puts "Destroying old records..."
+Comment.destroy_all
+Post.destroy_all
+Category.destroy_all
+Tag.destroy_all
+User.destroy_all
+puts "Old records destroyed."
+
+#-----Create Objects-----
+# 10 Users
+10.times do
+  User.create(name: Faker::DrWho.unique.character)
+end
+# 5 Catgories
+5.times do
+  Category.create(name: Faker::HarryPotter.unique.character)
+end
+# 5 Tags
+5.times do
+  Tag.create(name: Faker::LordOfTheRings.character)
+end
+# 5 Posts per User, 3 comments and 3 tags per Post
+User.find_each do |user|
+  5.times do
+    post = user.posts.create(title: Faker::StarWars.planet,
+                             body: Faker::StarWars.quote,
+                             category: Category.all.sample)
+    3.times do
+      post.comments.create(body: Faker::StarWars.wookie_sentence)
+      post.tag.create(tag: Tag.all.sample)
+    end
+  end
+end
+puts "New records created!"

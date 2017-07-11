2d4dc9f76e32d5625a36a4af53b2d9cfefcc4205
Ann Allan <ann.cristina.allan@gmail.com>
2017-07-10 22:46:15 -0400

    Fix seeds and migrations for 'The Basics' in Assoc Calisthenics to work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 
 #-----Destroy previous records-----
 puts "Destroying old records..."
@@ -20,14 +21,17 @@ puts "Old records destroyed."
 10.times do
   User.create(name: Faker::DrWho.unique.character)
 end
+puts "10 Users created"
 # 5 Catgories
 5.times do
   Category.create(name: Faker::HarryPotter.unique.character)
 end
+puts "5 Categories created"
 # 5 Tags
 5.times do
-  Tag.create(name: Faker::LordOfTheRings.character)
+  Tag.create(name: Faker::LordOfTheRings.unique.character)
 end
+puts "5 Tags created"
 # 5 Posts per User, 3 comments and 3 tags per Post
 User.find_each do |user|
   5.times do
@@ -35,9 +39,13 @@ User.find_each do |user|
                              body: Faker::StarWars.quote,
                              category: Category.all.sample)
     3.times do
-      post.comments.create(body: Faker::StarWars.wookie_sentence)
-      post.tag.create(tag: Tag.all.sample)
+      post.comments.create(body: Faker::StarWars.wookie_sentence, user_id: Faker::Number.between(1, 10))
+    end
+    3.times do
+      PostTag.create(post_id: post.id,
+                     tag_id: Faker::Number.between(1, 3))
     end
   end
+  puts "5 Posts with 3 Comments and 3 Tags created for each User"
 end
 puts "New records created!"

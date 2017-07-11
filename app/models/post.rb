e6c197d9ae362107eec93358a7f9bb53d43f4731
Ann Allan <ann.cristina.allan@gmail.com>
2017-07-10 23:23:25 -0400

    Update Dependents in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :category
-  has_many :comments
-  has_many :post_authorings, class_name: "UserPost"
+  has_many :comments, dependent: :destroy
+  has_many :post_authorings, class_name: "UserPost", dependent: :destroy
   has_many :authors, through: :post_authorings, class_name: "User", source: :user
   has_many :post_taggings, class_name: "PostTag"
-  has_many :tags, through: :post_taggings
+  has_many :tags, through: :post_taggings, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :post_authorings, class_name: "UserPost"
-  has_many :authored_posts, through: :post_authorings, class_name: "Post", source: :post
-  has_many :authored_comments, class_name: "Comment"
+  has_many :authored_posts, through: :post_authorings, class_name: "Post", source: :post, dependent: :destroy
+  has_many :authored_comments, class_name: "Comment", dependent: :destroy
   has_many :tags_on_authored_posts, through: :post_taggings, class_name: "Tag",  source: :tag
 end
